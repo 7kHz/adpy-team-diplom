b4320fa65092dc213e65029a145e1325009ba330
Maxim Uteshev <guitarheroe2012@ya.ru>
2023-04-13 13:18:27 +0300

    edir Vkinder_bot_1.py

--- Vkinder_bot_1.py
+++ Vkinder_bot_1.py
@@ -6,11 +6,13 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from dotenv import load_dotenv, find_dotenv
 import re
 from vk_data_exchange import vk_api_data
+from time import sleep
 
 load_dotenv(find_dotenv())
 token = os.getenv('ACCESS_TOKEN')
-input_data_list = []    #список входных данных полученных от пользователя для поиска
-user_id_list = []   #значение user_id
+input_data_list = []    # список входных данных полученных от пользователя для поиска
+user_id_list = []   # значение user_id
+intermidiate_list = [] # список временного хранения кандидатов
 chosen_candidate_list = []  #список избранных кандидатов
 
 # генерация клавиатуры чат-бота
@@ -19,7 +21,7 @@ def menu_keyboard():
     keyboard.add_button(label='\U00002605', color=VkKeyboardColor.PRIMARY)
     keyboard.add_button(label='\U0000279C', color=VkKeyboardColor.POSITIVE)
     keyboard.add_line()
-    keyboard.add_button(label='Показать \U00002605', color=VkKeyboardColor.SECONDARY)
+    keyboard.add_button(label='показать \U00002605', color=VkKeyboardColor.SECONDARY)
     return keyboard.get_keyboard()
 
 
@@ -41,26 +43,27 @@ def new_message(candidate_data=None):
                     write_msg(event.user_id,
                               'Добро пожаловать в Vkinder!\n\n'
                               'Введите данные для поиска через запятую в формате:\nВозраст, пол, город:')
-                    # chosen_candidate_list.clear()
                 elif request.lower() == 'стоп':
                     break
                 elif candidate_data is not None:
                     if request.lower() == '\U0000279C':
-                        generator_candidates(next(candidate_data))
+                        intermidiate_list.append(generator_candidates(next(candidate_data)))
                     elif request.lower() == '\U00002605':
-                        chosen_candidate_list.append(next(candidate_data))
-                    elif request.lower() == 'Показать \U00002605':
-                        pass
+                        chosen_candidate_list.append(intermidiate_list.pop())
+                        # вместо candidates вставить список избранных кандидатов
+                    # elif request.lower() == 'показать \U00002605':
+                    #     for candidate in candidates:
+                    #         generator_candidates(candidate)
                     else:
                         write_msg(user_id_list[0], 'Неопознанная команда!')
                 else:
                     if len(check_len_request) == 3:
                         input_data_list.append(request.title().strip())
                         return input_data_list
-                    elif (request.lower() == '\U0000279C' or '\U00002605') and len(check_len_request) == 0:
-                        write_msg(event.user_id,
-                                  'Добро пожаловать в Vkinder!\n\n'
-                                  'Введите данные для поиска через запятую в формате:\nВозраст, пол, город:')
+                    # elif (request.lower() == '\U0000279C' or '\U00002605') and len(check_len_request) == 0:
+                    #     write_msg(event.user_id,
+                    #               'Добро пожаловать в Vkinder!\n\n'
+                    #               'Введите данные для поиска через запятую в формате:\nВозраст, пол, город:')
                     else:
                         write_msg(event.user_id, 'Введен неверный формат данных! Повторите попытку:')
 
@@ -106,6 +109,7 @@ def generator_candidates(candidate_dict):
                                  f'photo{candidate_dict["id"]}_'
                                  f'{candidate_dict["photos_ids"][1]}',
                       keyboard=menu_keyboard())
+            return candidate_dict
         elif len(candidate_dict['photos_ids']) == 1:
             write_msg(user_id_list[0], f'{candidate_dict["first_name"]} '
                                        f'{candidate_dict["last_name"]}'
@@ -113,11 +117,13 @@ def generator_candidates(candidate_dict):
                       attachment=f'photo{candidate_dict["id"]}'
                                  f'_{candidate_dict["photos_ids"][0]}',
                       keyboard=menu_keyboard())
+            return candidate_dict
         elif len(candidate_dict['photos_ids']) == 0:
             write_msg(user_id_list[0], f'{candidate_dict["first_name"]} '
                                        f'{candidate_dict["last_name"]}'
                                        f'\n{candidate_dict["link"]}',
                       keyboard=menu_keyboard())
+            return candidate_dict
         else:
             write_msg(user_id_list[0], 'По вашему запросу ничего не найдено!')
 
@@ -128,6 +134,4 @@ if __name__ == '__main__':
     vk_search = vk_api_data.VKAPI()
     candidate_data = formatting_data(new_message())
     candidates = vk_search.search_candidates(candidate_data['age'], candidate_data['sex'], candidate_data['city'])
-    generator_candidates(next(candidates))
     new_message(candidates)
-    print(chosen_candidate_list)
